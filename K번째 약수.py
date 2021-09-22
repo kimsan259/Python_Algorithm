c60824d583d70e1a254ecf6b53c8946d3609af0e
kimsan4516 <kimsan4516@naver.com>
2021-09-22 18:13:02 +0900

    추가

--- K번째 약수.py
+++ K번째 약수.py
@@ -1,12 +1,9 @@
 import sys
 #sys.stdin=open("input.txt", "rt")
-n, k =map(int, input().split())
-cnt = 0
-for i in range(1, n+1):
-    if n%2==0:
-        cnt+=1
-    if cnt==k:
-        print(i)
-        break
-else:
-    print(-1)
+T = int(input())
+for t in range(T):
+    n,s,e,k = map(int, input().split())
+    a=list(map(int, input().split()))
+    a=a[s-1:e]
+    a.sort()
+    print("#%d %d" %(t+1, a[k-1]))
