2f651b324dbfdf235e939247cdba740b5814aee9
kimsan4516 <kimsan4516@naver.com>
2021-11-21 23:40:48 +0900

    탐색 시뮬:

---
+++ 곶감(모래시계).py
@@ -0,0 +1,25 @@
+import sys
+n = int(input())
+a = [list(map(int,input().split())) for _ in range(n)]
+m = int(input())
+for i in range(m):
+    h, t, k = map(int,input().split())
+    if t == 0:
+        for _ in range(k):
+            a[h-1].append(a[h-1].pop(0)) # 이 행의 제일앞에 요소를 제일 뒤에 추가시킨다.
+    else:
+        for _ in range(k):
+            a[h-1].insert(0, a[h-1].pop()) # 이 행의 제일 뒷 자료를 앞에 0번 인덱스에 넣는다.
+res = 0
+s = 0
+e = n-1
+for i in range(n):
+    for j in range(s, e+1):
+        res +=a[i][j]
+    if i<n//2:
+        s += 1
+        e -= 1
+    else:
+        s -= 1
+        e += 1
+print(res)
